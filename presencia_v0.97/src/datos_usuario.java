822e00807aec534f226703210bdf245b6937cf84
Marcos Rodriguez <deflagmator@gmail.com>
2012-11-20 10:36:24 +0100

    test2

--- presencia_v0.97/src/datos_usuario.java
+++ presencia_v0.97/src/datos_usuario.java
@@ -87,7 +87,7 @@ public class datos_usuario extends JFrame {
 	private JTable disfrute_horas_extras_table;
 
 	/**
-	 * Launch the application.
+	 * Launch the application. test github
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
