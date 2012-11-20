e468cea398040550e7d3ce92ebfec13e5c2d2dd2
Marcos Rodriguez <deflagmator@gmail.com>
2012-11-20 12:15:37 +0100

    Revert "a"
    This reverts commit dec6ad2a3ffb5867b00aaec893125e48638b8ecb.

--- presencia_v0.97/src/datos_usuario.java
+++ presencia_v0.97/src/datos_usuario.java
@@ -87,7 +87,7 @@ public class datos_usuario extends JFrame {
 	private JTable disfrute_horas_extras_table;
 
 	/**
-	 * Launch the application aaaa
+	 * Launch the application.test prueba de git subido.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
